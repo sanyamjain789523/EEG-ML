dc00562136e17edec86ad7260c962f4c1e366222
sanyam <sanyamjain789523@gmail.com>
2023-12-27 16:06:19 +0530

    minor changes

--- src/data_processing/data_addition.py
+++ src/data_processing/data_addition.py
@@ -1,16 +1,16 @@
 import pandas as pd
-from pathlib import Path
+# from pathlib import Path
 import os
 from io import BytesIO
-
+# from src.main import train_folder, test_folder
 from src.data_processing.data_processor import session_data_aggregation
 
-current_dir = Path.cwd()
+# current_dir = Path.cwd()
 
 # train_folder = current_dir / "src" / "data" / "training_data"
 # test_folder = current_dir / "src" / "data" / "test_data"
-train_folder = current_dir / "data" / "training_data"
-test_folder = current_dir / "data" / "test_data"
+# train_folder = current_dir / "data" / "training_data"
+# test_folder = current_dir / "data" / "test_data"
 
 
 def prepare_data_for_prediction(csv_file):
@@ -21,7 +21,7 @@ def prepare_data_for_prediction(csv_file):
     return aggregated_df
     
 
-def add_training_data_to_folder(csv_file, filename, is_adhd):
+def add_training_data_to_folder(csv_file, filename, is_adhd, train_folder):
     contents = csv_file.read()
     data = BytesIO(contents)
     df = pd.read_excel(data, sheet_name=[f"Session {i}" for i in range(1, 8)], index_col=None)
@@ -31,7 +31,7 @@ def add_training_data_to_folder(csv_file, filename, is_adhd):
     aggregated_df.to_csv(f"{train_folder}/{filename}", index=False)
 
 
-def add_test_data_to_folder(csv_file, filename, is_adhd):
+def add_test_data_to_folder(csv_file, filename, is_adhd, test_folder):
     contents = csv_file.read()
     data = BytesIO(contents)
     df = pd.read_excel(data, sheet_name=[f"Session {i}" for i in range(1, 8)], index_col=None)
@@ -41,14 +41,14 @@ def add_test_data_to_folder(csv_file, filename, is_adhd):
     aggregated_df.to_csv(f"{test_folder}/{filename}", index=False)
 
 
-def delete_file(filename, is_train):
+def delete_file(filename, is_train, train_folder, test_folder):
     if is_train:
         os.remove(f"{train_folder}/{filename}")
     else:
         os.remove(f"{test_folder}/{filename}")
 
 
-def list_files(is_train):
+def list_files(is_train, train_folder, test_folder):
     if is_train:
         return os.listdir(f"{train_folder}")
     else:
--- src/main.py
+++ src/main.py
@@ -1,7 +1,15 @@
 from src.modelling.train import predict_using_rf, train_models
 from src.data_processing.data_addition import add_test_data_to_folder, add_training_data_to_folder, delete_file, list_files, prepare_data_for_prediction
 from fastapi import FastAPI, UploadFile
+from pathlib import Path
 
+current_dir = Path.cwd()
+
+
+train_folder = current_dir / "src" / "data" / "training_data"
+test_folder = current_dir / "src" / "data" / "test_data"
+
+print("train_folder: ", train_folder)
 app = FastAPI()
 
 
@@ -12,40 +20,40 @@ def root():
 @app.post("/delete_data")
 def remove_file(filenames: list[str], is_train: bool):
     for file in filenames:
-        delete_file(file, is_train)
+        delete_file(file, is_train, train_folder, test_folder)
     return {"message": "files removed"}
 
 @app.get("/list_data")
 def list_file(is_train: bool):
-    files = list_files(is_train)
+    files = list_files(is_train, train_folder, test_folder)
     return {"files": files}
 
 @app.post("/upload_adhd_training_files")
 async def upload_adhd_training_files(files: list[UploadFile]):
     for file in files:
         file.filename = file.filename.replace("xlsx", "csv")
-        add_training_data_to_folder(file.file, file.filename, 1)
+        add_training_data_to_folder(file.file, file.filename, 1, train_folder)
     return {"filenames": [file.filename for file in files]}
 
 @app.post("/upload_non_adhd_training_files")
 async def upload_non_adhd_training_files(files: list[UploadFile]):
     for file in files:
         file.filename = file.filename.replace("xlsx", "csv")
-        add_training_data_to_folder(file.file, file.filename, 0)
+        add_training_data_to_folder(file.file, file.filename, 0, train_folder)
     return {"filenames": [file.filename for file in files]}
 
 @app.post("/upload_adhd_test_files")
 async def upload_adhd_test_files(files: list[UploadFile]):
     for file in files:
         file.filename = file.filename.replace("xlsx", "csv")
-        add_test_data_to_folder(file.file, file.filename, 1)
+        add_test_data_to_folder(file.file, file.filename, 1, test_folder)
     return {"filenames": [file.filename for file in files]}
 
 @app.post("/upload_non_adhd_test_files")
 async def upload_non_adhd_test_files(files: list[UploadFile]):
     for file in files:
         file.filename = file.filename.replace("xlsx", "csv")
-        add_test_data_to_folder(file.file, file.filename, 0)
+        add_test_data_to_folder(file.file, file.filename, 0, test_folder)
     return {"filenames": [file.filename for file in files]}
 
 @app.post("/train_model")
