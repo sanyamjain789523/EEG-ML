7fe02c899b01e47bad87327b8529a64009a5b8b2
sanyam <sanyamjain789523@gmail.com>
2023-12-29 10:36:09 +0530

    minor changes

--- src/main.py
+++ src/main.py
@@ -2,8 +2,7 @@ from src.modelling.train import predict_using_rf, train_models
 from src.data_processing.data_addition import add_test_data_to_folder, add_training_data_to_folder, delete_file, list_files, prepare_data_for_prediction
 from fastapi import FastAPI, UploadFile
 from pathlib import Path
-from fastapi.encoders import jsonable_encoder
-from fastapi.responses import JSONResponse
+
 
 
 # current_dir = os.getcwd()
@@ -25,60 +24,83 @@ def root():
 
 @app.post("/delete_data")
 def remove_file(filenames: list[str], is_train: bool):
-    for file in filenames:
-        delete_file(file, is_train, train_folder, test_folder)
+    try:
+        for file in filenames:
+            delete_file(file, is_train, train_folder, test_folder)
+    except Exception as e:
+        return {"Error": str(e)}
     return {"message": "files removed"}
 
 @app.get("/list_data")
 def list_file(is_train: bool):
-    files = list_files(is_train, train_folder, test_folder)
+    try:
+        files = list_files(is_train, train_folder, test_folder)
+    except Exception as e:
+        return {"Error": e}
     return {"files": files}
 
 @app.post("/upload_adhd_training_files")
 async def upload_adhd_training_files(files: list[UploadFile]):
-    for file in files:
-        file.filename = file.filename.replace("xlsx", "csv")
-        add_training_data_to_folder(file.file, file.filename, 1, train_folder)
+    try:
+        for file in files:
+            file.filename = file.filename.replace("xlsx", "csv")
+            add_training_data_to_folder(file.file, file.filename, 1, train_folder)
+    except Exception as e:
+        return {"Error": e}
     return {"filenames": [file.filename for file in files]}
 
 @app.post("/upload_non_adhd_training_files")
 async def upload_non_adhd_training_files(files: list[UploadFile]):
-    for file in files:
-        file.filename = file.filename.replace("xlsx", "csv")
-        add_training_data_to_folder(file.file, file.filename, 0, train_folder)
+    try:
+        for file in files:
+            file.filename = file.filename.replace("xlsx", "csv")
+            add_training_data_to_folder(file.file, file.filename, 0, train_folder)
+    except Exception as e:
+        return {"Error": e}
     return {"filenames": [file.filename for file in files]}
 
 @app.post("/upload_adhd_test_files")
 async def upload_adhd_test_files(files: list[UploadFile]):
-    for file in files:
-        file.filename = file.filename.replace("xlsx", "csv")
-        add_test_data_to_folder(file.file, file.filename, 1, test_folder)
+    try:
+        for file in files:
+            file.filename = file.filename.replace("xlsx", "csv")
+            add_test_data_to_folder(file.file, file.filename, 1, test_folder)
+    except Exception as e:
+        return {"Error": e}
     return {"filenames": [file.filename for file in files]}
 
 @app.post("/upload_non_adhd_test_files")
 async def upload_non_adhd_test_files(files: list[UploadFile]):
-    for file in files:
-        file.filename = file.filename.replace("xlsx", "csv")
-        add_test_data_to_folder(file.file, file.filename, 0, test_folder)
+    try:
+        for file in files:
+            file.filename = file.filename.replace("xlsx", "csv")
+            add_test_data_to_folder(file.file, file.filename, 0, test_folder)
+    except Exception as e:
+        return {"Error": e}
     return {"filenames": [file.filename for file in files]}
 
 @app.post("/train_model")
 async def train_model(algorithm: str):
-    metrics = train_models(algorithm)
-    # metrics = jsonable_encoder(metrics)
-    print(metrics)
+    try:
+        metrics = train_models(algorithm)
+        # metrics = jsonable_encoder(metrics)
+        print(metrics)
+    except Exception as e:
+        return {"Error": e}
     return {"metrics": {algorithm: metrics}}
     
 
 @app.post("/predict")
 async def predict(files: list[UploadFile], algorithm: str):
     predictions = {}
-    for file in files:
-        file.filename = file.filename.replace("xlsx", "csv")
-        pred_df = prepare_data_for_prediction(file.file)
-        pred = predict_using_rf(pred_df)
-        predictions[file.filename] = pred.item()
-    
+    try:
+        for file in files:
+            file.filename = file.filename.replace("xlsx", "csv")
+            pred_df = prepare_data_for_prediction(file.file)
+            pred = predict_using_rf(pred_df)
+            predictions[file.filename] = pred.item()
+    except Exception as e:
+        return {"Error": e}
     return predictions
     
 
